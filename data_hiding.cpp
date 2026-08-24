e3806e087d6f0ee0be94c8a795d229f6d8bda4ab
Rohit Bhattacharyya <rohitbhattacharyya07@gmail.com>
2026-08-24 11:29:57 +0530

    exp-5

--- data_hiding.cpp
+++ data_hiding.cpp
@@ -1,64 +1,64 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class BankAccount{
-    private:
+class BankAccount {
+private:
     int balance;
     int AccountNumber;
 
-    public:
-    
-    BankAccount(int AccNo , int bal){
+public:
+
+    BankAccount(int AccNo, int bal) {
         AccountNumber = AccNo;
         balance = bal;
     }
 
-    void Deposit(int input){
+    void Deposit(int input) {
         balance += input;
         cout << "Amount " << input << " Added Successfully" << endl;
     }
 
-    void Withdrawl(int amount){
-        balance -= amount;
 
-        if(balance < 0){
-            cout << "Insufficient Balance"<<endl;
+    void Withdrawal(int amount) {
+        if (amount > balance) {
+            cout << "Insufficient Balance" << endl;
         }
-        else{
+        else {
             balance -= amount;
-            cout << "Withdrawl Successfull"<<endl;
+            cout << "Withdrawal Successful" << endl;
         }
     }
 
-    void DisplayBalance(){
-        cout << "Current Balance is : " << balance << endl;
+
+    void DisplayBalance() {
+        cout << "Current Balance is: " << balance << endl;
     }
 };
 
-int main(){
-    int AccNo , Balance , Deposit , withdraw;
+int main() {
+
+    int AccNo, Balance, Deposit, withdraw;
 
     cout << "Enter Acc No: ";
     cin >> AccNo;
+
     cout << "Enter Balance: ";
     cin >> Balance;
 
+    
+    BankAccount BankAccount1(AccNo, Balance);
 
-    BankAccount BankAccount1(AccNo,Balance);
-
-    cout << "Amount to Deposit";
+    cout << "Amount to Deposit: ";
     cin >> Deposit;
 
     BankAccount1.Deposit(Deposit);
 
-    cout << "Amount to Withdraw";
+    cout << "Amount to Withdraw: ";
     cin >> withdraw;
-    
-    BankAccount1.Withdrawl(withdraw);
+
+    BankAccount1.Withdrawal(withdraw);
 
     BankAccount1.DisplayBalance();
 
-    
-    
-
+    return 0;
 }
