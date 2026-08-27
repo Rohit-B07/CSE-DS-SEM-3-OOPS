c641d63b374cd8ffc498aae7b28e9e83baf2aa3d
Rohit Bhattacharyya <rohitbhattacharyya07@gmail.com>
2026-08-27 10:26:43 +0530

    friend function complete

--- friend_function.cpp
+++ friend_function.cpp
@@ -60,27 +60,115 @@
 
 //FRIEND FUNC ACCESSING BOTH PRIVATE AND PUBLIC ELEMENT
 
+// #include <iostream>
+// using namespace std;
+// class Employee{
+//     long long salary;
+//     public:
+//     string name;
+
+//     Employee(string s, int n){
+//         name = s;
+//         salary = n;
+//     }
+
+//     friend void display(Employee e);
+// };
+
+// void display(Employee e){
+//     cout << "Name : "<<e.name<<endl;
+//     cout << "Salary : "<<e.salary<<endl;
+// }
+
+// int main(){
+//     Employee e("Rohit" , 800000000);
+//     display(e);
+// }
+
+
+// FRIEND FUNCTION WITH TWO DIFF CLASS
+
+// #include <iostream>
+// using namespace std;
+
+// class B;
+// class A{
+//     int x;
+
+//     public:
+//     A(){
+//         x=10;
+//     }
+
+//     friend void add(A,B);
+// };
+
+// class B{
+//     int y;
+
+//     public:
+//     B(){
+//         y=20;
+//     }
+
+//     friend void add(A,B);
+// };
+
+// void add (A a,B b){
+//     cout << a.x + b.y;
+// }
+
+// int main(){
+//     A a;
+//     B b;
+//     add(a,b);
+// }
+
+
+//FRIEND FUNCTION FOR ADDITION OF TWO OBJECTS;
+
 #include <iostream>
 using namespace std;
-class Employee{
-    long long salary;
-    public:
-    string name;
 
-    Employee(string s, int n){
-        name = s;
-        salary = n;
+class Number{
+    int value;
+    public:
+    Number(int v){
+        value = v;
     }
 
-    friend void display(Employee e);
+    friend Number add(Number n1 , Number n2);
+    void display(){
+        cout << "Value : " << value<<endl;
+    }
 };
 
-void display(Employee e){
-    cout << "Name : "<<e.name<<endl;
-    cout << "Salary : "<<e.salary<<endl;
+Number add (Number n1 , Number n2){
+    Number result(n1.value+n2.value);
+
+    return result;
 }
 
 int main(){
-    Employee e("Rohit" , 800000000);
-    display(e);
+    Number n1(6);
+    Number n2(8);
+
+    Number n3 = add(n1,n2);
+
+    n3.display();
+
 }
+
+
+/*
+/Feature                                        / Member Function   / Friend Function                                           /
+/-----------------------------------------------/-------------------/--------------------------------------------------------   /
+/CLASS MEMBER?                                  / YES               /   No                                                      /
+/Can access private data?                       / YES               /   YES                                                     /
+/Can access protected data?                     / YES               /   YES                                                     /
+/Has 'this' pointer?                            / YES               /   NO                                                      /
+/Called using object?                           / Usually Yes       /   NO                                                      /
+/Declared using 'friend'?                       / NO                /   YES                                                     /
+/Can access private data of multiple classes?   / NORMALLY NO       /   YES , IF FRIEND OF BOTH                                 /
+/common use                                     /Class operation    /   OPERATOR OVERLOADING , COOPERATION BETWEEN CLASSES      /
+*/
